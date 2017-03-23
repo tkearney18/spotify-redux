19da4a4b55e819e97472decb7097f29b0f45ffad
Troy Kearney <tkearney18@gmail.com>
2017-03-23 10:50:47 -0400

    Added the Result component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import axios from 'axios'
+import { Button } from 'react-bootstrap';
 
 let Result = ({ result }) => {
-    console.log( result)
     return(
-        <div>
-            <a href={ result.external_urls.spotify }><h2>{ result.name }</h2></a>
-            {!result.images.length ? <p>No Image Available</p> : <img src={ result.images[0].url } style={{ 
-                height: 50, width: 50
-            }} />}
-            <ul>
-                {result.genres.map(g =>
-                    <li key={g.i}>{g}</li>
-                )}
-            </ul>
+        <div className="row">
+            <div className="col-lg-6">
+                <a href={ result.external_urls.spotify }><h2>{ result.name }</h2></a>
+                {!result.images.length ? <p>No Image Available</p> : <img src={ result.images[0].url } style={{ 
+                    height: 50, width: 50
+                }} />}
+                <ul>
+                    {result.genres.map(g =>
+                        <li key={Math.random()}>{g}</li>
+                    )}
+                </ul>
+            </div>
+            <div className="<col-lg-2></col-lg-2>" style={{
+                 minHeight: 100, display: 'flex', alignItems: 'center'
+            }}>
+                <Button> Add to Favorites </Button>
+            </div>
         </div>
     )
 }
