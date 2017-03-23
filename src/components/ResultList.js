91a3899175b88490aa649fc80ec743b98bf34fbf
Troy Kearney <tkearney18@gmail.com>
2017-03-23 10:51:13 -0400

    Added the ResultList component

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import axios from 'axios'
 import Result from './Result'
 
 class ResultList extends React.Component {
     render(){
         return(
-            <div>
-                {this.props.results.map( result =>
-                    <Result result={result}/>)
-                }
+            <div className="row">
+                <div className="col-lg-8">
+                    {this.props.results.map( result =>
+                        <Result key={Math.random()} result={result}/>)
+                    }
+                </div>
             </div>
         )
     }
